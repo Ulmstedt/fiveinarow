6dc75b1cc01147baaec69d36ecd59636bc3a7e90
pzyber <pzyber@pzyber.net>
2015-12-29 12:55:19 +0100

    Various fixes.

---
+++ fiveinarow/JimmyAI.java
@@ -0,0 +1,32 @@
+package fiveinarow;
+
+import Pzyber.Loki.Gomoku.Loki;
+import Pzyber.Loki.Gomoku.LokiResult;
+
+import java.awt.*;
+
+public class JimmyAI extends Player {
+
+    private Loki loki;
+
+    public JimmyAI(int id, Game game) {
+        super(id, game);
+        int width = game.getWidth();
+        int height = game.getHeight();
+        pointGrid = new int[width][height];
+
+        // Add new Loki.
+        this.loki = game.loki;
+    }
+
+    @Override
+    public void playRound(){
+        // Let Loki decide what move to play.
+        int[][] board = game.getBoard();
+        LokiResult lr = loki.thinkOfAMove(board);
+        Point move = lr.getMove();
+        game.setTile(move.x, move.y, ID);
+        game.incrementRoundCount();
+        game.nextPlayer();
+    }
+}
